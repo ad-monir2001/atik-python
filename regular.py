97d9d6b6438e0dd24b00ffd7434d8772f07b70b0
Moniruzzaman <ad.monir2001@gmail.com>
2024-03-30 19:07:53 +0600

    some update on py file

--- regular.py
+++ regular.py
@@ -1,10 +1,7 @@
-f=int(input())
-if f>5:
-   f=f+1
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
         res=0
-         try:
+        try:
             if divisor==0:
              res= dividend//divisor
         except :
@@ -12,6 +9,5 @@ class Solution:
         if dividend >=1 or dividend<0 :
                 res=int(dividend//divisor)
         return res
-    my=Solution()
-    print(my.divide(int(input()),int(input())))
-continue
+my=Solution()
+print(my.divide(int(input()),int(input())))
