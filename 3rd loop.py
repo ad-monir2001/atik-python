707e6aa7fef3ac0e1e3dab25d13fe8217352453f
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-08 08:19:22 +0600

    su

--- 3rd loop.py
+++ 3rd loop.py
@@ -6,18 +6,5 @@
 #         return res
 # m=Solution()
 # print(m.complexNumberMultiply((input()),input()))
-# class Solution:
-#     def sumOfMultiples(self, n: int):
-#         c=0
-#         for i in range(1,n,3):
-#             c=c+1
-#             res = int (c + i)
-#             return int(res)
-# my=Solution()
-# print(my.sumOfMultiples(int(input())))
-p=0
-r=int(input())
-for i in range(3,r+1,3): 
-    n= p+ i
-    p=n
-    print(i) 
+
+
