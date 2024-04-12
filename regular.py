75369647f909fe7678710ba174e9fcfacd6dd4d3
Moniruzzaman <ad.monir2001@gmail.com>
2024-04-12 07:57:09 +0600

    update on regular

---
+++ regular.py
@@ -0,0 +1,6 @@
+from typing import Counter
+class Solution:
+    def majorityElement(self, nums):
+        return Counter(nums).most_common(6)[0][0]   
+m=Solution()
+print(m.majorityElement(list(input().split())))        
