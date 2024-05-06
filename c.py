eaad3b70678f81a8c79537b7507c392c5ef6acf8
Moniruzzaman <ad.monir2001@gmail.com>
2024-05-06 21:06:52 +0600

    solve done

--- c.py
+++ c.py
@@ -1,26 +1,43 @@
-import turtle 
-t=turtle.Turtle()
-t.hideturtle()
-t.color("green")
-t.begin_fill()
-for i in range(2):
-    
-    t.forward(150)
-    t.right(90)
-    t.forward(100)
-    t.right(90)
+# import turtle 
+# t=turtle.Turtle()
+# t.hideturtle()
+# t.color("green")
+# t.begin_fill()
+# for i in range(2): 
+#     t.forward(150)
+#     t.right(90)
+#     t.forward(100)
+#     t.right(90)
+# t.end_fill()
+# t.penup()
+# t.goto(90,-50)
+# t.pendown()
+# t.color("red")
+# t.begin_fill()
+# t.circle(20)
 
-t.end_fill()
-t.penup()
-t.goto(100,-50)
-t.pendown()
-t.color("red")
-t.begin_fill()
-t.circle(20)
-
-t.end_fill()
-turtle.exitonclick()
+# t.end_fill()
+# turtle.exitonclick()
 
 # ! HOME work
 
 # make a hexagonal (sorovuj)
+
+import turtle
+t=turtle.Turtle()
+t.hideturtle()
+t.color("green")
+t.begin_fill()
+t.goto(0,50)
+for i in range(6):
+  t.forward(100)
+  t.right(60)
+t.end_fill()
+t.penup()
+import math
+t.goto(50,-(math.sqrt(6400)))
+t.color("red")
+t.begin_fill()
+t.circle(50)
+t.end_fill()
+turtle.exitonclick()
