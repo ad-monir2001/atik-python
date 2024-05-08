82f9e3ca3bff48341568ab85f02b10770c95201e
Moniruzzaman <ad.monir2001@gmail.com>
2024-05-08 22:00:54 +0600

    prac

--- 1st string.py
+++ 1st string.py
@@ -1,6 +1,6 @@
 
 # #print("a\ts")"
-# #print("\n")#it print new line
+print("""PS C:\\Users\\User\\OneDrive\\Desktop\\atik-python> & c:/Users/User/OneDrive/Desktop/atik-python/myenv/Scripts/python.exe "c://Users//User//OneDrive//Desktop/atik-python//1st string.py\n""")#it print new line
 
 ##!DICTIONARY 
 # #It(d[0]) use to call any element in dictionary
@@ -12,5 +12,77 @@ DIC["DICr"]={"A":"ATIKUR","S":"RAHMAN"}#It(dic_name["what will I change"]) use t
 # print(DIC["DISTRICT"]);"==";print(DIC.get("DISTRICT"))
 # DIC.items() #*dict to tuple
 # del DIC["DICr"]
-for i in DIC:
-    print(i)
+# for i in DIC:
+#     print(i)
+import turtle
+# t=turtle.Turtle()
+# t.hideturtle()
+# t.color("green")
+# t.begin_fill()
+# t.penup()
+# t.goto(0,50)
+# t.pendown()
+# for i in range(6):
+#   t.forward(100)
+#   t.right(60)
+# t.end_fill()
+
+# import math
+
+# t.goto(50,-(math.sqrt(6400)))
+# t.color("red")
+# t.begin_fill()
+# t.circle(50)
+# t.end_fill()
+
+# t.penup()
+# t.goto(0,50)
+# t.forward(200)
+# t.pendown()
+# t.color("green")
+# t.begin_fill()
+# for i in range(6):
+#   t.forward(100)
+#   t.right(60)
+# t.end_fill()
+# t.goto(250,-(math.sqrt(6400)))
+# t.color("red")
+# t.begin_fill()
+# t.circle(50)
+# t.end_fill()
+# turtle.exitonclick()
+# import turtle
+# import random
+# t=turtle.Turtle()
+
+# t.penup()
+# t.goto(-100,50)
+# '#ff0000'
+# # t.speed(20)
+# t.color('#%0x' % random.randint(0, 2**24 - 1))
+# t.begin_fill()
+# t.circle(50)
+# t.end_fill()
+# t.color('green')
+# t.shape("turtle")
+# t.speed(2)
+# t.goto(-100,-150)
+# t.shape("turtle")
+# t.speed(2)
+# t.forward(280)
+# t.left(90)
+# t.forward(250)
+
+# t.forward(150)
+# t.speed()
+# t.goto(180,-190)
+# t.speed(2)
+# t.left(90)
+# t.forward(90)
+# t.goto(90,150)
+# t.color('#%0x' % random.randint(0, 2**24 - 1))
+# t.begin_fill()
+# t.circle(50)
+# t.end_fill()
+# t.goto(-10,25)
+# t.color("red")
--- c.py
+++ c.py
@@ -23,21 +23,51 @@
 
 # make a hexagonal (sorovuj)
 
+# import turtle
+# t=turtle.Turtle()
+# t.hideturtle()
+# t.color("green")
+# t.begin_fill()
+# t.penup()
+# t.goto(0,50)
+# t.pendown()
+# for i in range(6):
+#   t.forward(100)
+#   t.right(60)
+# t.end_fill()
+
+# import math
+
+# t.goto(50,-(math.sqrt(6400)))
+# t.color("red")
+# t.begin_fill()
+# t.circle(50)
+# t.end_fill()
+# turtle.exitonclick()
+
 import turtle
+import random
 t=turtle.Turtle()
-t.hideturtle()
-t.color("green")
-t.begin_fill()
-t.goto(0,50)
-for i in range(6):
-  t.forward(100)
-  t.right(60)
-t.end_fill()
-t.penup()
-import math
-t.goto(50,-(math.sqrt(6400)))
-t.color("red")
+t.color('#%06x' % random.randint(0, 2**24 - 1))
 t.begin_fill()
 t.circle(50)
 t.end_fill()
+t.color('#9999ff')
+t.goto(0,-80)
+t.shape("turtle")
+t.forward(200)
+# import turtle
+# t=turtle.Turtle()
+# import random
+# t.penup()
+# t.goto(-170,150)
+# t.write("Create Your Free Account","center",None,"25pt bold")
+# t.goto(-140,120)
+# t.write("'Let's get started! Your free account","center",None,"18pt bold")
+# t.goto(-140,100)
+# t.write(" will let you create trinkets, see ","center",None,"18pt bold")
+# t.goto(-140,80)
+# t.write("interaction statistics for each trinket","center",None,"15pt bold")
+# t.goto(-140,60)
+# t.write(" you create, and more.","center",None,"18pt bold")
 turtle.exitonclick()
