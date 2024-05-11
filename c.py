da36c430e7475c4424860b7c676961a8a06c9962
Moniruzzaman <ad.monir2001@gmail.com>
2024-05-11 11:30:39 +0600

    Home work done of turtle

--- c.py
+++ c.py
@@ -1,12 +1,3 @@
-
-# ! HOME work
-#DRAW A RECTANGLE WITH INT 
-#circle input(color),input(radious)
-
-
-# import turtle
-# import random
-# t=turtle.Turtle()
 # t.color('#%06x' % random.randint(0, 2**24 - 1))
 # t.begin_fill()
 # t.circle(50)
@@ -20,14 +11,39 @@
 # import random
 # t.penup()
 # t.goto(-170,150)
+# t.write("you create, and more.", align="center", font=("" ,20, ""))
+# ! HOME work
+#!Home wark 1
+# # *Draw a rectangle .The length of that rectangle is a and width is b. a and b are an integer number which will be chosen by the user.
+import turtle
+t=turtle.Turtle()
+a=int(input("Please take an integer number : "))
+b=int(input("Please take an another integer number : "))
+if a!=b:
+    for i in range(2):
 
-# t.write("Create Your Free Account","center",None,"25pt bold")
-# t.goto(-140,120)
-# t.write("'Let's get started! Your free account","center",None,"18pt bold")
-# t.goto(-140,100)
-# t.write(" will let you create trinkets, see ","center",None,"18pt bold")
-# t.goto(-140,80)
-# t.write("interaction statistics for each trinket","center",None,"15pt bold")
-# t.goto(-140,60)
-# t.write(" you create, and more.","center",None,"18pt bold")
+        t.pencolor("red")
+        t.width(2)
+        t.forward(a)
+        t.left(90)
+        t.forward(b)
+        t.left(90)
+else:
+    print("To draw a rectangle, the length and width will not be equal.")
+
+# #!Home wark 2
+#*Draw a circle with a radius chosen by the user, and  filled the circle with a color which also chosen by the user 
+# import turtle
+# t=turtle.Turtle()
+# t.hideturtle()
+# r=int(input("radius of circle : "))
+# color=input("Choose a color which color will filled with a circle : ")
+# t.pencolor("black")
+# t.width(2)
+# t.begin_fill()
+# t.circle(r)
+# t.color(color)
+# t.end_fill()
+# turtle.done()
+
 
