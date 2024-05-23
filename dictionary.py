8a68ae157ac36ed3b32aea8e7f05492fff413400
Moniruzzaman <ad.monir2001@gmail.com>
2024-05-23 11:44:44 +0600

    dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,18 +1,68 @@
 #DICt
+# user={
+#     "Name":"Atikur",
+#     "age":17,
+#     "gender":"male",
+#     "IS_maried":False,
+#     "hobbies":["reading","playing","programming"],
+#     "c":{"country":"Bangladesh",
+#         "city":"Bogura",
+#         "village":"chotochapra"}
+#     }
+
+# for a,b in user.items():
+#     print(f"{a}:{b}")
+
+# user["Name"]="Rahman"
+# print(user)
+# print(user.get("Name"))
+ 
+
 user={
-    "Name":"Atikur",
-    "age":17,
-    "gender":"male",
-    "IS_maried":False,
-    "hobbies":["reading","playing","programming"],
-    "c":{"country":"Bangladesh",
-        "city":"Bogura",
-        "village":"chotochapra"}
+    "user1":{
+        "Name":"Atikur Rahman",
+        "Gander":"Male",
+        "Date of birth":"30-05-2007",
+        "Area":{
+                "Country":"Bangladesh",
+                "District":"Bogura",
+                "Village":"cotochapra"
+                }
+
+    },
+    "user2":{
+        "Name":"Virat Kohli",
+        "Gander":"Male",
+        "Date of birth":"10-11-195",
+        "Area":{
+                "Country":"India",
+                "District":"Bangalaru",
+                "Village":"vid"
+        }
+    },
+
+    "user3":{
+        "Name":"Babar Azam",
+        "Gander":"Male",
+        "Date of birth":"01-08-1992",
+        "Area":{
+                "Country":"Pakistan",
+                "District":"United islamabad",
+                "Village":"Kua"
+        }
     }
+}
 
-for a,b in user.items():
-    print(f"{a}:{b}")
+a=input("Enter Area/Name/Gander/Date of birth : ")
+if a=="Area":
+    s=user[input("Enter any name user1/user2/user3 : ")][a]
+    for key,value in s.items():
+        print(f"{key}:{value}")
 
-user["Name"]="Rahman"
-print(user)
-print(user.get("Name"))
+
+elif a=="Name":
+    print(user[input("Enter any name user1/user2/user3 : ")][a])
+elif a=="Gander":
+    print(user[input("Enter any name user1/user2/user3 : ")][a])
+else:
+    print(user[input("Enter any name user1/user2/user3 : ")][a])
