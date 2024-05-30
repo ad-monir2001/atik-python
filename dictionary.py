66174ab51b658fd32da8f635274f679fbff88569
Moniruzzaman <ad.monir2001@gmail.com>
2024-05-30 21:57:09 +0600

    s

--- dictionary.py
+++ dictionary.py
@@ -18,103 +18,94 @@
 # print(user.get("Name"))
  
 
-user={
-    "user1":{
-        "Name":"Atikur Rahman",
-        "Gander":"Male",
-        "Date of birth":"30-05-2007",
-        "Area":{
-                "Country":"Bangladesh",
-                "District":"Bogura",
-                "Village":"cotochapra"
-                }
-    },
-    "user2":{
-        "Name":"Virat Kohli",
-        "Gander":"Male",
-        "Date of birth":"10-11-195",
-        "Area":{
-                "Country":"India",
-                "District":"Bangalaru",
-                "Village":"vid"
-        }
-    },
-    "user3":{
-        "Name":"Babar Azam",
-        "Gander":"Male",
-        "Date of birth":"01-08-1992",
-        "Area":{
-                "Country":"Pakistan",
-                "District":"United islamabad",
-                "Village":"Kua"
-        }
-    }
-}
-#=======================================
-# choose=[ "users sort information","users full information"]
-# c=input(f"please choose anything,, {choose[0]}/{choose[1]} : ")
-# #=======================================
-# if c==choose[0]:
-#     u=input("Enter any user user1/user2/user3 : ")
-#     a=input("Enter Area/Name/Gander/Date of birth : ")
-#     if a.capitalize()=="Area":
-#         s=user[u][a.capitalize()]
-#         for key,value in s.items():
-#             print(f"{key}:{value}")
-#     elif a.capitalize()=="Name"or"Gander"or"Date of birth":
-#         print(user[u][a.capitalize()])
-# #=======================================
-# else:
-#     for k,v in user[input("Enter any user user1/user2/user3 : ")].items():
-#         print(f'{k}:{v}')
-#=======================================
-# us=input("user name ")
-# ar=input("area ")
-# v=input("v")
-us=["user1","user2","user3"]
-for i in us:
-    for e,f in user[i]["Area"].items():
-        print(e)
-        print(e["Village"])
 
 
-print(user["user1"]["Area"]["Village"])
-
-
-
-# country={ 
-
-#   "Barisal":{
-#       "Barguna","Barisal","Bhola","Jhalokati","Patuakhali", "Pirojpur"
-#       },
-
-#   "Chittagong":{
-#       "Bandarban","Brahmanbaria","Chandpur","Chittagong","Comilla","Cox's Bazar","Feni","Khagrachhari","Lakshmipur", "Noakhali","Rangamati"
-#                 },
-
-#   "Dhaka":{
-#       "Dhaka","Faridpur","Gazipur","Gopalganj","Kishoreganj","Madaripur","Manikganj","Munshiganj","Rajbari","Narayanganj","Narsingdi","Tangail","Shariatpur"
-#       },
-
-#   "Khulna":{
-#       "Bagerhat", "Chuadanga","Jessore","Jhenaidah","Khulna",     "Kushtia","Magura","Meherpur",    "Narail","Satkhira"
-#       },
-
-#   "Mymensingh":{
-#       "Jamalpur", "Mymensingh","Netrakona","Sherpur"
-#       },
-
-#   "Rajshahi":{
-#         "Bogra":{
-#           "Bogura sadar","sherpur","dhunat","Sariakandi",
-#             },
-#         "Chapainawabganj":{},"Joypurhat":{},"Naogaon":{},"Natore":{},"Pabna":{},"Rajshahi":{},"Sirajganj":{}
-#         },
-
-#   "Rangpur":{"Dinajpur", "Gaibandha","Kurigram", "Lalmonirhat","Nilphamari","Panchagarh","Rangpur","Thakurgaon"},
-
-#   "Sylhet":{"Habiganj","Moulvibazar","Sunamganj","Sylhet"}
-
+# user={
+#     "user1":{
+#         "Name":"Atikur Rahman",
+#         "Gander":"Male",
+#         "Date of birth":"30-05-2007",
+#         "Area":{
+#                 "Country":"Bangladesh",
+#                 "District":"Bogura",
+#                 "Village":"cotochapra"
+#                 }
+#     },
+#     "user2":{
+#         "Name":"Virat Kohli",
+#         "Gander":"Male",
+#         "Date of birth":"10-11-195",
+#         "Area":{
+#                 "Country":"India",
+#                 "District":"Bangalaru",
+#                 "Village":"vid"
+#         }
+#     },
+#     "user3":{
+#         "Name":"Babar Azam",
+#         "Gander":"Male",
+#         "Date of birth":"01-08-1992",
+#         "Area":{
+#                 "Country":"Pakistan",
+#                 "District":"United islamabad",
+#                 "Village":"Kua"
+#         }
+#     }
 # }
-# for k,v in country[input("division of bangladesh: ")].items():
-#     print(f"{k}:{v}")
+# while True:
+#     #=======================================
+#     choose=[ "users sort information","users full information"]
+#     c=input(f"please choose anything,, {choose[0]}/{choose[1]} : ")
+#     #=======================================
+#     if c==f'{c}':
+#         u=input("Enter any user user1/user2/user3 : ")
+#         a=input("Enter Area/Name/Gander/Date of birth : ")
+#         if a.capitalize()=="Area":
+#             s=user[u][a.capitalize()]
+#             for key,value in s.items():
+#                 print(f"{key}:{value}")
+#         elif a.capitalize()=="Name"or"Gander"or"Date of birth":
+#             print(user[u][a.capitalize()])
+#     #=======================================
+#     else:
+#         for k,v in user[input("Enter any user user1/user2/user3 : ")].items():
+#             print(f'{k}:{v}')
+
+#     us=list(map(str,input("which village do you want to see ? :").split(" ")))
+#     for i in us:
+#         if i in user:
+#             print(user[i]["Area"]["Village"])
+#         else:
+#             break
+
+
+# O/x puzzle game
+
+while True:
+    def count(x):
+
+        try:
+            return 0/x
+        except ZeroDivisionError as a:
+            print(a)
+
+
+        except TypeError as m:
+            print(m)
+        except ValueError as e :
+            print(e)
+        finally :
+            print(0/x)
+
+    print(count(int(input())))
+
+
+
+def mu(x):
+    try:
+        return 0/x
+    except ZeroDivisionError as z : 
+        print("yor are win the game ")
+
+
+print(mu(int(input())))
