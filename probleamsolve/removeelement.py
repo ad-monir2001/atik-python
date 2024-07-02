2b65e1ffa74cc5de2769d11b23f3aa9ab7f59299
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2024-07-02 16:42:52 +0600

    up

--- probleamsolve/removeelement.py
+++ probleamsolve/removeelement.py
@@ -1,26 +1,15 @@
-# class Solution:
-#     def removeDuplicates(self, nums: list) -> int:
-#         for i in nums:
-#             if nums.count(i)>1:
-#                 nums.remove(i)
-#         return nums
-    
-    
-# s=Solution()
-# print(s.removeDuplicates([1,1,2]))
-
 class Solution:
-    def removeDuplicates(self, nums: list) -> int:
-        set1=set(nums)
-        return (set1)
-    
-s=Solution()
-print(s.removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
-# list=[1,1,2]
-# for i in list:
-#     if list.count(i)>1:
-#         list.remove(i)
-        
-        
-# print(list)
+    def addToArrayForm(self, num: list[int], k: int) -> list[int]:
+        list1=[]
+        list2=[]
+        if len(num)>=1 and len(num)<=10**4:
+            for i in num:
+                list1.append(str(i))
+            e=int(''.join(list1))+k
+            s=str(e)
+            for j in s:
+                list2.append(int(j))
+
+            return list2
+
 
