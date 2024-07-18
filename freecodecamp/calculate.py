f30b8d17dbd195507b0cf09d27d3510e4b9f59c9
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2024-07-18 07:34:04 +0600

    774

--- freecodecamp/calculate.py
+++ freecodecamp/calculate.py
@@ -1,14 +1,24 @@
 
 
-x=float(input("x ; "))
-y=float(input('y ; '))
-z=round(x+y)
-print(z)
-print(f'{z:22,}') 
+# x=float(input("x ; "))
+# y=float(input('y ; '))
+# z=round(x+y)
+# print(z)
+# print(f'{z:22,}') 
 
 # (values:object,sep:str|None=" ",end:str|None="\n",file:SupportsWrite[str]|None=None,flush:Literal[False] =False)->None
 # (values:object,sep:str|None=" ",end:str|None="\n", file:_SupportsWriteAndFlush[str]|None=None,flush:bool)->None
 #Prints the values to a stream,or to sys.stdout by default.
 
-a=x/y
-print(f'{a:.9f}')# after int number it count 9 digit.
+# a=x/y
+# print(f'{a:.9f}')# after int number it count 9 digit.
+
+
+def main():
+    name=input('what is your name ? ')
+    hello(name)
+    
+def hello(int):
+    print('hello' , int)
+int='world'
+main()
--- freecodecamp/frist.py
+++ freecodecamp/frist.py
@@ -1,4 +1,12 @@
-print('Atikur Rahman','Atik',sep='""""')
-name=input("Nmae : ")
-first,last=name.split("   ")
-print(f'{first},{last}')
+# print('Atikur Rahman','Atik',sep='""""')
+# name=input("Nmae : ")
+# first,last=name.split("   ")
+# print(f'{first},{last}')
+
+names=[]
+for _ in range(3):
+    names.append(input('what is your name? '))
+    
+    
+for name in sorted(names):
+    print(f'hello {name}')
